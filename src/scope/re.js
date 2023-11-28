606d6f0e918df73d741e8affcdc3931b5ccb6c2c
Andrea Rivas <andrerivas25@gmail.com>
2023-11-28 14:16:40 -0300

    reasignar y redeclarar variables / strict mode

---
+++ src/scope/re.js
@@ -0,0 +1,34 @@
+// var
+var firstName; // Declarar, Undefined
+firstName = 'Andrea'; //Asignar
+
+var lastName = 'Rivas'; // Declarar / asignar
+lastName = 'Ruiz'; // reasignar
+console.log(lastName);
+
+var secondName = 'Alejandra'; // Declarar / asignar
+var secondName = 'Maria'; // Redeclarar / reasignar
+console.log(secondName);
+
+
+// let
+let fruit = 'Apple'; // Declarar / asignar
+fruit = 'Banana'; // reasignar
+console.log(fruit);
+
+// let fruit = 'Starwberry';
+// console.log(fruit);
+
+
+// const
+const animal = 'Dog'; // Declarar / asignar
+animal = 'Cat'; // reasignar
+// const animal = 'Bird';
+// console.log(animal);
+
+const vehicles = [];
+vehicles.push('🚓');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
